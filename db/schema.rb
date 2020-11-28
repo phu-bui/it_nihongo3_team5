7f2b20e6814582d7043a8db56fe409cc608b62f6
Ubuntu <ubuntu@ip-172-31-87-1.ec2.internal>
2020-11-28 03:22:31 +0000

    Add book

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_28_030249) do
+ActiveRecord::Schema.define(version: 2020_11_28_031800) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,16 @@ ActiveRecord::Schema.define(version: 2020_11_28_030249) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.integer "number_of_pages"
+    t.float "price"
+    t.string "author"
+    t.string "pulisher"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "mangas", force: :cascade do |t|
